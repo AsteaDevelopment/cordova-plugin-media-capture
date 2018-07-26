ab563bb3b704c965477bd08769bac5db63e37973
rdantonio <asteadev@gmail.com>
2018-07-26 09:52:23 -0400

    Added quality option for iOS picker with unique name

--- src/ios/CDVCapture.m
+++ src/ios/CDVCapture.m
@@ -220,9 +220,10 @@
         options = [NSDictionary dictionary];
     }
 
-    // options could contain limit, duration and mode
+    // options could contain limit, duration, quality and mode
     // taking more than one video (limit) is only supported if provide own controls via cameraOverlayView property
     NSNumber* duration = [options objectForKey:@"duration"];
+    NSNumber* pickerQuality = [options objectForKey:@"picker_quality"];
     NSString* mediaType = nil;
 
     if ([UIImagePickerController isSourceTypeAvailable:UIImagePickerControllerSourceTypeCamera]) {
@@ -266,6 +267,21 @@
         // iOS 4.0
         if ([pickerController respondsToSelector:@selector(cameraCaptureMode)]) {
             pickerController.cameraCaptureMode = UIImagePickerControllerCameraCaptureModeVideo;
+
+            if (pickerQuality) {
+                switch([pickerQuality intValue]) {
+                    case 0: 
+                        pickerController.videoQuality = UIImagePickerControllerQualityTypeLow;
+                        break;
+                    case 1: 
+                        pickerController.videoQuality = UIImagePickerControllerQualityTypeHigh;
+                        break;
+                    default:
+                        pickerController.videoQuality = UIImagePickerControllerQualityTypeMedium;
+                        break;
+               }
+            }
+
             // pickerController.videoQuality = UIImagePickerControllerQualityTypeHigh;
             // pickerController.cameraDevice = UIImagePickerControllerCameraDeviceRear;
             // pickerController.cameraFlashMode = UIImagePickerControllerCameraFlashModeAuto;
